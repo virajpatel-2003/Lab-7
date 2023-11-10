d03084471bee33ee544a450a92368565926b7d90
Viraj <virajpatel3@cmail.carleton.ca>
2023-11-10 15:48:39 -0500

    Complete

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,17 +1,7 @@
 public class ResidentialSite extends Site {
     double getBillableAmount() {
-        double base = getBaseAmount();
-        double tax = getTaxAmount(base);
-        return base + tax;
-    }
-
-    private static double getTaxAmount(double base) {
-        double tax = base * Site.TAX_RATE;
-        return tax;
-    }
-
-    private double getBaseAmount() {
         double base = _units * _rate;
-        return base;
+        double tax = base * Site.TAX_RATE;
+        return base + tax;
     }
 }
