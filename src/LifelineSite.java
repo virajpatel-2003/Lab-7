eb31c66984944b7add73c805b55307d74aa94ee9
Viraj <virajpatel3@cmail.carleton.ca>
2023-11-10 15:49:16 -0500

    Complete

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,16 +1,16 @@
 public class LifelineSite extends Site{
         double getBillableAmount() {
-            return super.getBillableAmount();
-        }
+            double base = getBaseAmount();
+            double tax = getTaxAmount();
+            return base + tax;
+    }
 
-     @Override
      double getTaxAmount() {
         double base = getBaseAmount();
         double tax = base * Site.TAX_RATE * 0.2;
         return tax;
     }
 
-     @Override
      double getBaseAmount() {
         double base = _units * _rate * 0.5;
         return base;
